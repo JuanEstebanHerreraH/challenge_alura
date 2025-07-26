2597afd89bdadb543dc3c7faa911c7ef56ed4483
esteban <estebanherreraherrera8gmail.com>
2025-07-26 15:30:18 -0500

    Codigo acabado

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let ListFriends = [];
+let addfriend;
+
+function GeneratedText(element, text) {
+    let el = document.querySelector(element); 
+    el.innerHTML = text; 
+}
+
+function AddFriend() {
+    addfriend = document.getElementById('amigo').value;
+
+    if (!addfriend) { 
+        alert('Amigo Secreto: No se escribió ningún nombre.');
+    } else if (ListFriends.includes(addfriend)) {
+        alert('Ese amigo ya fue añadido.');
+    } else {
+        ListFriends.push(addfriend);
+        alert('Amigo añadido correctamente.');
+    }
+}
+
+function loterryFriend() {
+    if (ListFriends.length === 0) {
+        alert('No hay amigos en la lista para elegir.');
+        return;
+    }
+
+    let random = Math.floor(Math.random() * ListFriends.length);
+    let selectedFriend = ListFriends[random];
+
+    GeneratedText('h2', `El amigo seleccionado fue: ${selectedFriend}`);
+}
+
+
